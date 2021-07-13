2e3ca25797d6a4e65be57705a129e46f1a275946
Kirill Apanasiuk <pogudo.tatyana@mail.ru>
2021-07-13 13:35:12 +0300

    test commit

---
+++ SemaphoreTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Reader reader = new Reader(i);
+                Console.WriteLine($"Current reader iteration {i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Reader
+    {
+        static Semaphore sem = new Semaphore(3, 3);
+        int count = 3;
+        Thread myThread;
+
+        public Reader(int i)
+        {
+            myThread = new Thread(Read);
+            myThread.Name = $"Reader {i.ToString()}";
+            myThread.Start();
+        }
+
+        public void Read()
+        {
+            while (count > 0)
+            {
+                sem.WaitOne();
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} входит в библиотеку");
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} читает");
+                Thread.Sleep(5000);
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} покидает библиотеку");
+
+                sem.Release();
+
+                count--;
+                Console.WriteLine($"{Thread.CurrentThread.Name} и его каунт = {count}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
